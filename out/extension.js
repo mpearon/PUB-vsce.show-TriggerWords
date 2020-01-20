a92776421884ce42d2f78ff0b454fd8bda7fd14d
Matthew A. Pearon <mpearon@users.noreply.github.com>
2020-01-19 21:52:16 -0600

    Multiple changes - Import   - Removed Disposable, TextDocument   - Added Range - Activate   - Removed initial construct comments   - Added logic to loop through array of objects to parse document text

--- out/extension.js
+++ out/extension.js
@@ -1,26 +1,96 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode_1 = require("vscode");
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('show-triggerwords has been initialized');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
+    console.log('mpearon.vsce.show-triggerwords has been initialized');
+    // No editor is open, cancel load
+    if (!vscode_1.window.activeTextEditor)
+        return;
+    let updateDecorations = function (useHash = false) {
+        // No active edit is open
+        if (!vscode_1.window.activeTextEditor)
+            return;
+        // Display a StatusBar message
+        vscode_1.window.setStatusBarMessage('Show Trigger Words activated');
+        // Read in document text
+        let editorText = vscode_1.window.activeTextEditor.document.getText();
+        // Set Decoration Options
+        var decorationOptions = [
+            {
+                name: 'error',
+                expression: '^(error|fail(ure)?)',
+                decoration: {
+                    color: 'white',
+                    backgroundColor: 'red',
+                    fontWeight: 'bold'
+                }
+            },
+            {
+                name: 'warning',
+                expression: '^warn(ing)?',
+                decoration: {
+                    color: "black",
+                    backgroundColor: "orange",
+                    fontWeight: "bold"
+                }
+            },
+            {
+                name: 'information',
+                expression: '^info(rmation)?',
+                decoration: {
+                    color: "white",
+                    backgroundColor: "blue",
+                    fontWeight: "bold"
+                }
+            },
+            {
+                name: 'success',
+                expression: '^succe(ssful|eded|ss)',
+                decoration: {
+                    color: "white",
+                    backgroundColor: "green",
+                    fontWeight: "bold"
+                }
+            }
+        ];
+        // Loop through each decoration option
+        decorationOptions.forEach(option => {
+            console.log('Show-TriggerWords: Processing ' + option.name);
+            let optionDecoration = vscode_1.window.createTextEditorDecorationType(option.decoration);
+            let optionRanges = [];
+            let optionMatch;
+            let optionExpression = new RegExp(option.expression, 'gim');
+            if (vscode_1.window.activeTextEditor) {
+                while (optionMatch = optionExpression.exec(editorText)) {
+                    let optionStartIndex = vscode_1.window.activeTextEditor.document.positionAt(optionMatch.index);
+                    let optionEndIndex = vscode_1.window.activeTextEditor.document.positionAt(optionMatch.index + optionMatch[0].length);
+                    let optionRange = (new vscode_1.Range(optionStartIndex, optionEndIndex));
+                    optionRanges.push(optionRange);
+                    continue;
+                }
+                ;
+                vscode_1.window.activeTextEditor.setDecorations(optionDecoration, optionRanges);
+            }
+            ;
+        });
+    };
+    // Standard Initialization
+    if (vscode_1.window.activeTextEditor) {
+        updateDecorations();
+    }
+    // The open file has changed
+    vscode_1.window.onDidChangeActiveTextEditor(editor => {
+        if (editor) {
+            updateDecorations();
+        }
+    }, null, context.subscriptions);
+    // The user calls the 'Show Trigger Words' command from the command palette
     let disposable = vscode_1.commands.registerCommand('extension.showTriggerWords', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode_1.window.showInformationMessage('showTriggerWords');
+        updateDecorations();
     });
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
-// this method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,101 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import {
 	window,
 	commands,
-	Disposable,
 	ExtensionContext,
-	TextDocument
+	Range
 } from 'vscode';
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
+	console.log('mpearon.vsce.show-triggerwords has been initialized');
+	// No editor is open, cancel load
+	if (!window.activeTextEditor)
+		return;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('show-triggerwords has been initialized');
+	let updateDecorations = function (useHash = false) {
+		// No active edit is open
+		if (!window.activeTextEditor)
+			return;
+		// Display a StatusBar message
+		window.setStatusBarMessage('Show Trigger Words activated');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+		// Read in document text
+		let editorText = window.activeTextEditor.document.getText();
+
+		// Set Decoration Options
+		var decorationOptions = [
+			{
+				name: 'error',
+				expression: '^(error|fail(ure)?)',
+				decoration: {
+					color: 'white',
+					backgroundColor: 'red',
+					fontWeight: 'bold'
+				}
+			},
+			{
+				name: 'warning',
+				expression: '^warn(ing)?',
+				decoration: {
+					color: "black",
+					backgroundColor: "orange",
+					fontWeight: "bold"
+				}
+			},
+			{
+				name: 'information',
+				expression: '^info(rmation)?',
+				decoration: {
+					color: "white",
+					backgroundColor: "blue",
+					fontWeight: "bold"
+				}
+			},
+			{
+				name: 'success',
+				expression: '^succe(ssful|eded|ss)',
+				decoration: {
+					color: "white",
+					backgroundColor: "green",
+					fontWeight: "bold"
+				}
+			}
+		]
+
+		// Loop through each decoration option
+		decorationOptions.forEach(option => {
+			console.log('Show-TriggerWords: Processing ' + option.name);
+			let optionDecoration = window.createTextEditorDecorationType(option.decoration);
+			let optionRanges = [];
+			let optionMatch;
+			let optionExpression = new RegExp(option.expression, 'gim');
+			if (window.activeTextEditor) {
+				while (optionMatch = optionExpression.exec(editorText)) {
+					let optionStartIndex = window.activeTextEditor.document.positionAt(optionMatch.index);
+					let optionEndIndex = window.activeTextEditor.document.positionAt(optionMatch.index + optionMatch[0].length);
+					let optionRange = (new Range(optionStartIndex, optionEndIndex));
+					optionRanges.push(optionRange);
+					continue;
+				};
+				window.activeTextEditor.setDecorations(optionDecoration, optionRanges);
+			};
+		});
+	};
+
+	// Standard Initialization
+	if (window.activeTextEditor) {
+		updateDecorations();
+	}
+
+	// The open file has changed
+	window.onDidChangeActiveTextEditor(editor => {
+		if (editor) {
+			updateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	// The user calls the 'Show Trigger Words' command from the command palette
 	let disposable = commands.registerCommand('extension.showTriggerWords', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		window.showInformationMessage('showTriggerWords');
+		updateDecorations();
 	});
-
 	context.subscriptions.push(disposable);
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
