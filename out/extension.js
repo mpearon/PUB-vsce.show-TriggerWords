526004c0e225cf314656eb1264b8ea150455a642
Matthew A. Pearon <mpearon@users.noreply.github.com>
2020-01-21 23:33:26 -0600

    AutoUpdate Functionality - Restructured to add AutoUpdate functionality - Prepped to move to user contribution model

--- out/extension.js
+++ out/extension.js
@@ -1,15 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const vscode_1 = require("vscode");
+// Define the color options - will be moved to a separate file and be accessible via contributions 
+const color1Word = { /* red */ backgroundColor: '#ff0000', color: 'black', fontWeight: 'bolder' };
+const color1Line = { /* red */ backgroundColor: '#ff000030', isWholeLine: true };
+const color2Word = { /* blue */ backgroundColor: '#007fff', color: 'black', fontWeight: 'bolder' };
+const color2Line = { /* blue */ backgroundColor: '#007fff30', isWholeLine: true };
+const color3Word = { /* yellow */ backgroundColor: '#ffff00', color: 'black', fontWeight: 'bolder' };
+const color3Line = { /* yellow */ backgroundColor: '#ffff0030', isWholeLine: true };
+const color4Word = { /* green */ backgroundColor: '#00ff00', color: 'black', fontWeight: 'bolder' };
+const color4Line = { /* green */ backgroundColor: '#00ff0030', isWholeLine: true };
+// Build the decorationTypes
+const color1WordType = vscode_1.window.createTextEditorDecorationType(color1Word);
+const color1LineType = vscode_1.window.createTextEditorDecorationType(color1Line);
+const color2WordType = vscode_1.window.createTextEditorDecorationType(color2Word);
+const color2LineType = vscode_1.window.createTextEditorDecorationType(color2Line);
+const color3WordType = vscode_1.window.createTextEditorDecorationType(color3Word);
+const color3LineType = vscode_1.window.createTextEditorDecorationType(color3Line);
+const color4WordType = vscode_1.window.createTextEditorDecorationType(color4Word);
+const color4LineType = vscode_1.window.createTextEditorDecorationType(color4Line);
 function activate(context) {
     console.log('mpearon.vsce.show-triggerwords has been initialized');
     // No editor is open, cancel load
     if (!vscode_1.window.activeTextEditor)
         return;
+    // Decoration update function
     let updateDecorations = function (bypassLanguageCheck = false) {
-        // No active edit is open
-        if (!vscode_1.window.activeTextEditor)
-            return;
         if (bypassLanguageCheck == false) {
             if ((vscode_1.window.activeTextEditor.document.languageId) !== 'log') {
                 return;
@@ -17,103 +33,107 @@ function activate(context) {
         }
         // Read in document text
         let editorText = vscode_1.window.activeTextEditor.document.getText();
-        // Set Decoration Options
+        // Define object per expression - will be moved to a separate file and be accessible via contributions
         var decorationOptions = [
             {
                 name: 'error',
                 expression: 'err(or)?|fail(ure)?|crit(ical)?',
-                lineDecoration: {
-                    backgroundColor: '#FF000030'
-                },
-                wordDecoration: {
-                    color: 'black',
-                    fontWeight: 'bolder',
-                    backgroundColor: '#FF0000'
-                }
-            },
-            {
-                name: 'warning',
-                expression: 'warn(ing)?',
-                lineDecoration: {
-                    backgroundColor: '#FFFF0030'
-                },
-                wordDecoration: {
-                    color: 'black',
-                    fontWeight: 'bolder',
-                    backgroundColor: '#FFFF00'
-                }
+                decoration: 'color1'
             },
             {
                 name: 'information',
                 expression: 'info(rmation)?',
-                lineDecoration: {
-                    backgroundColor: '#007FFF30'
-                },
-                wordDecoration: {
-                    color: 'black',
-                    fontWeight: 'bolder',
-                    backgroundColor: '#007FFF'
-                }
+                decoration: 'color2'
+            },
+            {
+                name: 'warning',
+                expression: 'warn(ing)?',
+                decoration: 'color3'
             },
             {
                 name: 'success',
                 expression: 'succe(ssful|eded|ss)',
-                lineDecoration: {
-                    backgroundColor: '#00ff0030'
-                },
-                wordDecoration: {
-                    color: 'black',
-                    fontWeight: "bolder",
-                    backgroundColor: '#00ff00'
-                }
+                decoration: 'color4'
             }
         ];
         // Loop through each decoration option
         decorationOptions.forEach(option => {
-            console.log('Show-TriggerWords: Processing ' + option.name);
-            let optionWordDecoration = vscode_1.window.createTextEditorDecorationType(option.wordDecoration);
-            let optionWordRanges = [];
-            let optionWord;
-            let optionLineDecoration = vscode_1.window.createTextEditorDecorationType(option.lineDecoration);
+            console.debug('Show-TriggerWords: Processing ' + option.name);
+            // Update Highlight Line Range
             let optionLineRanges = [];
             let optionLine;
             let optionLineExpression = new RegExp(('.+(' + option.expression + ').+'), 'gim');
-            let optionWordExpression = new RegExp(option.expression, 'gim');
-            if (vscode_1.window.activeTextEditor) {
-                while (optionLine = optionLineExpression.exec(editorText)) {
-                    // Higlight Line
-                    let lineStart = vscode_1.window.activeTextEditor.document.positionAt(optionLine.index);
-                    let lineStop = vscode_1.window.activeTextEditor.document.positionAt(optionLine.index + optionLine[0].length);
-                    let lineRange = (new vscode_1.Range(lineStart, lineStop));
-                    optionLineRanges.push(lineRange);
-                    // Highlight Word
-                    while (optionWord = optionWordExpression.exec(editorText)) {
-                        let wordStart = vscode_1.window.activeTextEditor.document.positionAt(optionWord.index);
-                        let wordStop = vscode_1.window.activeTextEditor.document.positionAt(optionWord.index + optionWord[0].length);
-                        let wordRange = (new vscode_1.Range(wordStart, wordStop));
-                        optionWordRanges.push(wordRange);
-                        continue;
-                    }
-                    ;
-                    continue;
-                }
-                ;
-                // Highlight the line
-                vscode_1.window.activeTextEditor.setDecorations(optionLineDecoration, optionLineRanges);
-                // Highlight the match
-                vscode_1.window.activeTextEditor.setDecorations(optionWordDecoration, optionWordRanges);
+            while (optionLine = optionLineExpression.exec(editorText)) {
+                let lineStart = vscode_1.window.activeTextEditor.document.positionAt(optionLine.index);
+                let lineStop = vscode_1.window.activeTextEditor.document.positionAt(optionLine.index + optionLine[0].length);
+                let lineRange = (new vscode_1.Range(lineStart, lineStop));
+                optionLineRanges.push(lineRange);
+                continue;
             }
             ;
+            // Update Highlight Word Range
+            let optionWordRanges = [];
+            let optionWord;
+            let optionWordExpression = new RegExp(option.expression, 'gim');
+            while (optionWord = optionWordExpression.exec(editorText)) {
+                let wordStart = vscode_1.window.activeTextEditor.document.positionAt(optionWord.index);
+                let wordStop = vscode_1.window.activeTextEditor.document.positionAt(optionWord.index + optionWord[0].length);
+                let wordRange = (new vscode_1.Range(wordStart, wordStop));
+                optionWordRanges.push(wordRange);
+                continue;
+            }
+            ;
+            // Apply the correct decorations based on the option's decoration value
+            switch (option.decoration) {
+                case 'color1':
+                    {
+                        vscode_1.window.activeTextEditor.setDecorations(color1LineType, optionLineRanges);
+                        vscode_1.window.activeTextEditor.setDecorations(color1WordType, optionWordRanges);
+                    }
+                    ;
+                case 'color2':
+                    {
+                        vscode_1.window.activeTextEditor.setDecorations(color2LineType, optionLineRanges);
+                        vscode_1.window.activeTextEditor.setDecorations(color2WordType, optionWordRanges);
+                    }
+                    ;
+                case 'color3':
+                    {
+                        vscode_1.window.activeTextEditor.setDecorations(color3LineType, optionLineRanges);
+                        vscode_1.window.activeTextEditor.setDecorations(color3WordType, optionWordRanges);
+                    }
+                    ;
+                case 'color4':
+                    {
+                        vscode_1.window.activeTextEditor.setDecorations(color4LineType, optionLineRanges);
+                        vscode_1.window.activeTextEditor.setDecorations(color4WordType, optionWordRanges);
+                    }
+                    ;
+            }
         });
     };
-    // Standard Initialization
-    if (vscode_1.window.activeTextEditor) {
-        updateDecorations();
-    }
-    // The open file has changed
-    vscode_1.window.onDidChangeActiveTextEditor(editor => {
-        if (editor) {
+    // Prevents the updateDecorations function from triggering too often
+    var timeout;
+    function triggerUpdateDecorations() {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(() => {
             updateDecorations();
+        }, 500);
+    }
+    if (vscode_1.window.activeTextEditor) {
+        triggerUpdateDecorations();
+    }
+    // The user switched to another document
+    vscode_1.window.onDidChangeVisibleTextEditors(function (editor) {
+        updateDecorations();
+    }, null, context.subscriptions);
+    // Content of document changed
+    vscode_1.workspace.onDidChangeTextDocument(function (event) {
+        let activeEditor = vscode_1.window.activeTextEditor;
+        if (activeEditor && event.document === activeEditor.document) {
+            triggerUpdateDecorations();
         }
     }, null, context.subscriptions);
     // The user calls the 'Show Trigger Words' command from the command palette
--- src/extension.ts
+++ src/extension.ts
@@ -2,129 +2,143 @@ import {
 	window,
 	commands,
 	ExtensionContext,
-	Range
+	Range,
+	workspace
 } from 'vscode';
+
+// Define the color options - will be moved to a separate file and be accessible via contributions 
+const color1Word = { /* red */ backgroundColor: '#ff0000', color: 'black', fontWeight: 'bolder' };
+const color1Line = { /* red */ backgroundColor: '#ff000030', isWholeLine: true };
+const color2Word = { /* blue */ backgroundColor: '#007fff', color: 'black', fontWeight: 'bolder' };
+const color2Line = { /* blue */ backgroundColor: '#007fff30', isWholeLine: true };
+const color3Word = { /* yellow */ backgroundColor: '#ffff00', color: 'black', fontWeight: 'bolder' };
+const color3Line = { /* yellow */ backgroundColor: '#ffff0030', isWholeLine: true };
+const color4Word = { /* green */ backgroundColor: '#00ff00', color: 'black', fontWeight: 'bolder' };
+const color4Line = { /* green */ backgroundColor: '#00ff0030', isWholeLine: true };
+
+// Build the decorationTypes
+const color1WordType = window.createTextEditorDecorationType(color1Word);
+const color1LineType = window.createTextEditorDecorationType(color1Line);
+const color2WordType = window.createTextEditorDecorationType(color2Word);
+const color2LineType = window.createTextEditorDecorationType(color2Line);
+const color3WordType = window.createTextEditorDecorationType(color3Word);
+const color3LineType = window.createTextEditorDecorationType(color3Line);
+const color4WordType = window.createTextEditorDecorationType(color4Word);
+const color4LineType = window.createTextEditorDecorationType(color4Line);
+
 export function activate(context: ExtensionContext) {
 	console.log('mpearon.vsce.show-triggerwords has been initialized');
 	// No editor is open, cancel load
 	if (!window.activeTextEditor)
 		return;
-
+	// Decoration update function
 	let updateDecorations = function (bypassLanguageCheck = false) {
-		// No active edit is open
-		if (!window.activeTextEditor)
-			return;
 		if( bypassLanguageCheck == false ){
-			if((window.activeTextEditor.document.languageId) !== 'log'){
+			if((window.activeTextEditor!.document.languageId) !== 'log'){
 				return
 			}
-		}
-
+		}		
 		// Read in document text
-		let editorText = window.activeTextEditor.document.getText();
+		let editorText = window.activeTextEditor!.document.getText();
 
-		// Set Decoration Options
+		// Define object per expression - will be moved to a separate file and be accessible via contributions
 		var decorationOptions = [
 			{
 				name: 'error',
 				expression: 'err(or)?|fail(ure)?|crit(ical)?',
-				lineDecoration: {
-					backgroundColor: '#FF000030'
-				},
-				wordDecoration: {
-					color: 'black',
-					fontWeight: 'bolder',
-					backgroundColor: '#FF0000'
-				}
-			},
-			{
-				name: 'warning',
-				expression: 'warn(ing)?',
-				lineDecoration: {
-					backgroundColor: '#FFFF0030'
-				},
-				wordDecoration: {
-					color: 'black',
-					fontWeight: 'bolder',
-					backgroundColor: '#FFFF00'
-				}
+				decoration: 'color1'
 			},
 			{
 				name: 'information',
 				expression: 'info(rmation)?',
-				lineDecoration: {
-					backgroundColor: '#007FFF30'
-				},
-				wordDecoration: {
-					color: 'black',
-					fontWeight: 'bolder',
-					backgroundColor: '#007FFF'
-				}
+				decoration: 'color2'
+			},
+			{
+				name: 'warning',
+				expression: 'warn(ing)?',
+				decoration: 'color3'
 			},
 			{
 				name: 'success',
 				expression: 'succe(ssful|eded|ss)',
-				lineDecoration: {
-					backgroundColor: '#00ff0030'
-				},
-				wordDecoration: {
-					color: 'black',
-					fontWeight: "bolder",
-					backgroundColor: '#00ff00'
-				}
+				decoration: 'color4'
 			}
 		]
 
 		// Loop through each decoration option
 		decorationOptions.forEach(option => {
-			console.log('Show-TriggerWords: Processing ' + option.name);
-			let optionWordDecoration = window.createTextEditorDecorationType(option.wordDecoration);
-			let optionWordRanges = [];
-			let optionWord;
-			let optionLineDecoration = window.createTextEditorDecorationType(option.lineDecoration);
+			console.debug('Show-TriggerWords: Processing ' + option.name);
+
+			// Update Highlight Line Range
 			let optionLineRanges = [];
 			let optionLine;
 			let optionLineExpression = new RegExp(('.+(' + option.expression + ').+'),'gim');
-			let optionWordExpression = new RegExp(option.expression,'gim');
-			if (window.activeTextEditor) {
-				while (optionLine = optionLineExpression.exec(editorText)) {
-					// Higlight Line
-					let lineStart = window.activeTextEditor.document.positionAt(optionLine.index);
-					let lineStop = window.activeTextEditor.document.positionAt(optionLine.index + optionLine[0].length);
-					let lineRange = (new Range(lineStart, lineStop));
-					optionLineRanges.push(lineRange);
-					// Highlight Word
-					while (optionWord = optionWordExpression.exec(editorText)) {
-						let wordStart = window.activeTextEditor.document.positionAt(optionWord.index);
-						let wordStop = window.activeTextEditor.document.positionAt(optionWord.index + optionWord[0].length);
-						let wordRange = (new Range(wordStart, wordStop));
-						optionWordRanges.push(wordRange);
-						continue;
-						
-					};
-					continue;
-				};
-				// Highlight the line
-				window.activeTextEditor.setDecorations(optionLineDecoration, optionLineRanges);
-				// Highlight the match
-				window.activeTextEditor.setDecorations(optionWordDecoration, optionWordRanges);
+			while (optionLine = optionLineExpression.exec(editorText)) {
+				let lineStart = window.activeTextEditor!.document.positionAt(optionLine.index);
+				let lineStop = window.activeTextEditor!.document.positionAt(optionLine.index + optionLine[0].length);
+				let lineRange = (new Range(lineStart, lineStop));
+				optionLineRanges.push(lineRange);
+				continue;
 			};
+
+			// Update Highlight Word Range
+			let optionWordRanges = [];
+			let optionWord;
+			let optionWordExpression = new RegExp(option.expression,'gim');
+			while (optionWord = optionWordExpression.exec(editorText)) {
+				let wordStart = window.activeTextEditor!.document.positionAt(optionWord.index);
+				let wordStop = window.activeTextEditor!.document.positionAt(optionWord.index + optionWord[0].length);
+				let wordRange = (new Range(wordStart, wordStop));
+				optionWordRanges.push(wordRange);
+				continue;
+			};
+
+			// Apply the correct decorations based on the option's decoration value
+			switch(option.decoration){
+				case 'color1'	:	{
+					window.activeTextEditor!.setDecorations(color1LineType, optionLineRanges);
+					window.activeTextEditor!.setDecorations(color1WordType, optionWordRanges);
+				};
+				case 'color2'	:	{
+					window.activeTextEditor!.setDecorations(color2LineType, optionLineRanges);
+					window.activeTextEditor!.setDecorations(color2WordType, optionWordRanges);
+				};
+				case 'color3'	:	{
+					window.activeTextEditor!.setDecorations(color3LineType, optionLineRanges);
+					window.activeTextEditor!.setDecorations(color3WordType, optionWordRanges);
+				};
+				case 'color4'	:	{
+					window.activeTextEditor!.setDecorations(color4LineType, optionLineRanges);
+					window.activeTextEditor!.setDecorations(color4WordType, optionWordRanges);
+				};
+			}
 		});
 	};
-
-	// Standard Initialization
-	if (window.activeTextEditor) {
-		updateDecorations();
+	// Prevents the updateDecorations function from triggering too often
+	var timeout: NodeJS.Timeout;
+    function triggerUpdateDecorations() {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(() => {
+            updateDecorations();
+        }, 500);
 	}
-
-	// The open file has changed
-	window.onDidChangeActiveTextEditor(editor => {
-		if (editor) {
-			updateDecorations();
-		}
-	}, null, context.subscriptions);
-
-	// The user calls the 'Show Trigger Words' command from the command palette
+    if (window.activeTextEditor) {
+        triggerUpdateDecorations();
+    }
+	// The user switched to another document
+    window.onDidChangeVisibleTextEditors(function (editor) {
+        updateDecorations();
+    }, null, context.subscriptions);
+	// Content of document changed
+    workspace.onDidChangeTextDocument(function (event) {
+        let activeEditor = window.activeTextEditor;
+        if (activeEditor && event.document === activeEditor.document) {
+            triggerUpdateDecorations();
+        }
+    }, null, context.subscriptions);
+  	// The user calls the 'Show Trigger Words' command from the command palette
 	let disposable = commands.registerCommand('extension.showTriggerWords', () => {
 		updateDecorations(true);
 	});
